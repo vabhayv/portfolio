7365779e0d5cd4b0dc1692ea1ce9340a5d681f64
Abhay Lodhi <156104058+vabhayv@users.noreply.github.com>
2024-03-27 00:34:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(
+ anchor => {
+  anchor.addEventListener('click', function(e) {
+   e.preventDefault();
+
+   document.querySelector(this.getAttribute(
+    'href')).scrollIntoView({
+    behavior: 'smooth'
+   });
+  });
+ });
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+ let i;
+ let slides = document.getElementsByClassName(
+  "certificate-slide");
+ for (i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none";
+ }
+ slideIndex++;
+ if (slideIndex > slides.length) {
+  slideIndex = 1;
+ }
+ slides[slideIndex - 1].style.display = "block";
+ setTimeout(showSlides,
+ 3000); // Change image every 3 seconds
+}
+
+function changeSlide(n) {
+ slideIndex += n;
+ showSlides();
+}
+document.querySelector('.menu-toggle')
+ .addEventListener('click', function() {
+  document.querySelector('.nav-links').classList
+   .toggle('nav-active');
+ });

1e39cbfb59e9025e7ec9f1bb1d64b6e46c15c6d2
Abhay Lodhi <156104058+vabhayv@users.noreply.github.com>
2025-03-09 01:41:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,11 @@
-// Smooth scrolling for anchor links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault(); // Prevent default anchor behavior
+const hamburger = document.querySelector(
+ '.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector(
+ '.header .nav-bar .nav-list ul');
+const header = document.querySelector(
+ '.header.container');
 
-        // Scroll smoothly to the target section
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-
-        // Close the navigation menu by removing the 'nav-active' class
-        document.querySelector('.nav-links').classList.remove('nav-active');
-    });
-});
-
-// Slideshow functionality
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    let slides = document.getElementsByClassName("certificate-slide");
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 3000); // Change image every 3 seconds
-}
-
-function changeSlide(n) {
-    slideIndex += n;
-    showSlides();
-}
-
-// Toggle navigation menu for mobile devices
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-    document.querySelector('.nav-links').classList.toggle('nav-active'); // Toggle 'nav-active' class
+hamburger.addEventListener('click', () => {
+ hamburger.classList.toggle('active');
+ mobile_menu.classList.toggle('active');
 });
